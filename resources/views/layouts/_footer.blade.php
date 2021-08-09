66ecc4ed3a714db71c1680939e21c6a4825eb4cf
mango_baidu <944872615@qq.com>
2021-08-09 20:56:57 +0800

    first commit

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,22 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            <!-- 由
+            <a href="https://github.com/MangoXiaoda/larabbs" target="_blank">
+                MangoXiaoda
+            </a>
+            设计和编码
+            <span style="color: #e27575;font-size: 14px;">
+                ❤
+            </span> -->
+            <a href="https://beian.miit.gov.cn/" target="_blank">
+                粤ICP备2021094481号
+            </a>
+        </p>
+        <p class="float-right">
+            <a href="mailto:mangoxiaoda@gmail.com">
+                联系我们
+            </a>
+        </p>
+    </div>
+</footer>
---
+++ resources/views/topics/_sidebar.blade.php
@@ -0,0 +1,39 @@
+<div class="card ">
+  <div class="card-body">
+    <a class="btn btn-block" aria-label="Left Align">
+      欢迎光临本站
+    </a>
+  </div>
+</div>
+
+  <div class="card mt-4">
+    <div class="card-body active-users pt-2">
+      <div class="text-center mt-1 mb-0 text-muted">名言</div>
+      <hr class="mt-2">
+        <a class="media mt-2">
+        最困难的事情就是认识自己
+        </a>
+        <a class="media mt-2">
+        有勇气承担命运这才是英雄好汉
+        </a>
+        <a class="media mt-2">
+        自知之明是最难得的知识
+        </a>
+    </div>
+  </div>
+
+  <div class="card mt-4">
+    <div class="card-body pt-2">
+      <div class="text-center mt-1 mb-0 text-muted">推荐书籍</div>
+      <hr class="mt-2 mb-3">
+        <a class="media mt-1">
+            小王子
+        </a>
+        <a class="media mt-1">
+            杀死一只知更鸟
+        </a>
+        <a class="media mt-1">
+            摆渡人
+        </a>
+    </div>
+  </div>
